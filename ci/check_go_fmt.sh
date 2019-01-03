fd1217e8b02c9a9747bcc14045fb9b183cf8d230
kinsey40 <nickk15@hotmail.co.uk>
2019-01-03 20:29:05 +0000

    Altered the check fmt file

--- ci/check_go_fmt.sh
+++ ci/check_go_fmt.sh
@@ -1,13 +1,12 @@
-!/bin/bash
+#!/bin/bash
 # This script is used by the CI to check if the code is gofmt formatted.
 
 set -euo pipefail
 
-GOFMT_DIFF=$(IFS=$'\n'; gofmt -d $( find . -type f -name '*.go' ) )
-if [[ -n "${GOFMT_DIFF}" ]]; then
-    echo "${GOFMT_DIFF}"
-    echo
-    echo "The go source files aren't gofmt formatted."
+if [ -z "$(gofmt -l $( find . -type f -name '*.go' ) )" ]; then
+    echo "Gofmt correct"
+else
+    echo "The go source files aren't formatted correctly."
     exit 1
 fi
 
